658708dfd4c9e26be684ac350d6b040b829ca7ec
inf148124 <konrad.graja@gmail.com>
2023-05-09 22:10:45 +0200

    Sfinalizowany pasek nawigacji bez responsywnosci

--- app/src/main/java/com/example/mychef/MainActivity.kt
+++ app/src/main/java/com/example/mychef/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.example.mychef
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mychef.ui.theme.MyChefTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // use arrayadapter and define an array
-        val arrayAdapter: ArrayAdapter<*>
-        val users = arrayOf(
-            "Virat Kohli", "Rohit Sharma", "Steve Smith",
-            "Kane Williamson", "Ross Taylor"
-        )
-        // access the listView from xml file
-        var mListView = findViewById<ListView>(R.id.recipeList)
-        arrayAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, users)
-        mListView.adapter = arrayAdapter
+        setContent {
+            MainBody()
+        }
+//        setContentView(R.layout.activity_main)
+//        // use arrayadapter and define an array
+//        val arrayAdapter: ArrayAdapter<*>
+//        val users = arrayOf(
+//            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+//            "Kane Williamson", "Ross Taylor"
+//        )
+//        // access the listView from xml file
+//        var mListView = findViewById<ListView>(R.id.recipeList)
+//        arrayAdapter = ArrayAdapter(this,
+//            android.R.layout.simple_list_item_1, users)
+//        mListView.adapter = arrayAdapter
 
 
     }
-
+    @Preview(
+        showBackground = true,
+        showSystemUi = true,
+        name = "Full Body preview"
+    )
+    @Composable
+    fun MainBody()
+    {
+        NavMenu()
+    }
 }
 
