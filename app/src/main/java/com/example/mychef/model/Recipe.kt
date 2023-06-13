fc7d5ed86f9f2151495b86cf589cf907b3697c29
inf148124 <konrad.graja@gmail.com>
2023-06-13 19:55:05 +0200

    Added timer

--- app/src/main/java/com/example/mychef/model/Recipe.kt
+++ app/src/main/java/com/example/mychef/model/Recipe.kt
@@ -2,6 +2,6 @@ package com.example.mychef.model
 
 
 data class Recipe(var category: String, var name: String, var ingredients: List<String>,
-             var steps: List<String>, var image: Int) {
+             var steps: List<String>, var image: Int, var times: List<Long>) {
 
 }
--- app/src/main/java/com/example/mychef/view/RecipeDetail.kt
+++ app/src/main/java/com/example/mychef/view/RecipeDetail.kt
@@ -2,13 +2,25 @@ package com.example.mychef.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -17,7 +29,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import com.example.mychef.data.DataProvider
+import kotlinx.coroutines.delay
 
 @Composable
 fun DetailRecipe(name: String?) {
@@ -66,5 +80,72 @@ fun DetailRecipe(name: String?) {
                 modifier = Modifier.padding(start = 16.dp)
             )
         }
+        recipe.times.forEach { timer ->
+            Timer(timer)
+        }
+    }
+}
+@Composable
+fun Timer(
+    timeBegin: Long
+)
+{
+    var currentTime by remember {
+        mutableStateOf(timeBegin)
+    }
+    var isTimerStart by remember {
+        mutableStateOf(false)
+    }
+    LaunchedEffect(key1 = currentTime, key2 = isTimerStart) {
+        if(currentTime > 0 && isTimerStart) {
+            delay(100L)
+            currentTime -= 100L
+        }
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+
+    Button(
+        onClick = {
+                    if(currentTime <= 0L) {
+                        currentTime = timeBegin
+                        isTimerStart = true
+                    } else {
+                        isTimerStart = !isTimerStart
+                    }
+                  },
+        modifier = Modifier
+                        .padding(5.dp)
+                        .size(width = 25.dp, height = 25.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (!isTimerStart || currentTime <= 0L) {
+                Color.Green
+            } else {
+                Color.Red
+            }
+        ),
+        shape = RoundedCornerShape(28.dp),
+        contentPadding = PaddingValues(5.dp)
+    ) {
+        Icon(
+            if(!isTimerStart || currentTime <= 0L)
+            {
+                Icons.Rounded.PlayArrow
+            } else
+            {
+                Icons.Rounded.Done
+            },
+            modifier = Modifier.size(20.dp),
+            contentDescription = "time icon",
+            tint = Color.Unspecified
+        )
+    }
+    Text(
+        text = (currentTime / 60000L).toString() + "min " + ((currentTime % 60000L) / 1000).toString() + "sec",
+        modifier = Modifier.padding(start = 2.dp),
+        fontSize = 14.sp,
+        color = Color.Black
+    )
     }
 }
